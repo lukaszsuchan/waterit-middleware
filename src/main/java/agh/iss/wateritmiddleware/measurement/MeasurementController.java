b675f99fc57274cb0b65c0a55cd5677aa0dedd0f
LukaszSuchan <lukasz.suchan@interia.eu>
2023-05-06 18:23:23 +0200

    fix(param): correct adnotation

--- src/main/java/agh/iss/wateritmiddleware/measurement/MeasurementController.java
+++ src/main/java/agh/iss/wateritmiddleware/measurement/MeasurementController.java
@@ -3,10 +3,7 @@ package agh.iss.wateritmiddleware.measurement;
 import agh.iss.wateritmiddleware.measurement.model.MeasurementDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/measurement")
@@ -16,7 +13,7 @@ public class MeasurementController {
     private final MeasurementService measurementService;
 
     @GetMapping("/latest")
-    public ResponseEntity<MeasurementDto> getLatestMeasurement(@RequestAttribute Long zoneId) {
+    public ResponseEntity<MeasurementDto> getLatestMeasurement(@RequestParam Long zoneId) {
         return ResponseEntity.ok(measurementService.getLatestMeasurementByZoneId(zoneId));
     }
 }
