08de87fb2ee5ede28dd092dd1858dfdc4c810c51
LukaszSuchan <lukasz.suchan@interia.eu>
2023-06-29 10:39:36 +0200

    Update MeasurementPercentageMapper.java

--- src/main/java/agh/iss/wateritmiddleware/measurement/mapper/MeasurementPercentageMapper.java
+++ src/main/java/agh/iss/wateritmiddleware/measurement/mapper/MeasurementPercentageMapper.java
@@ -18,8 +18,8 @@ public class MeasurementPercentageMapper {
                 .lightIntensity(mapDataToPercentage(BigDecimal.ZERO, BigDecimal.valueOf(1000), measurement.getLightIntensity()))
                 .temperature(measurement.getTemperature())
                 .rainfall(measurement.getRainfall())
-                .humidity(measurement.getHumidity().divide(BigDecimal.valueOf(100)).setScale(2, RoundingMode.HALF_UP))
-                .moistureHumidity(measurement.getMoistureHumidity().divide(BigDecimal.valueOf(100)).setScale(2, RoundingMode.HALF_UP))
+                .humidity(measurement.getHumidity().divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP))
+                .moistureHumidity(measurement.getMoistureHumidity().divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP))
                 .airPurity(mapDataToPercentage(BigDecimal.valueOf(50), BigDecimal.valueOf(1000), measurement.getAirPurity()))
                 .build();
     }
